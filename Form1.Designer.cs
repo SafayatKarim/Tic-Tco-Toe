678db132922bc6821896f287979dd7fe4cbc98a8
Tic Toc Toe <safayat.karim369@gmail.com>
2019-09-12 11:05:42 +0600

    C# Language

---
+++ Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace tic_toc_toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A1 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.C3 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnPlayAgain = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // A1
+            // 
+            this.A1.Location = new System.Drawing.Point(0, 0);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(80, 66);
+            this.A1.TabIndex = 0;
+            this.A1.UseVisualStyleBackColor = true;
+            this.A1.Click += new System.EventHandler(this.A1_Click);
+            // 
+            // A2
+            // 
+            this.A2.Location = new System.Drawing.Point(86, 0);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(80, 66);
+            this.A2.TabIndex = 1;
+            this.A2.UseVisualStyleBackColor = true;
+            this.A2.Click += new System.EventHandler(this.A2_Click);
+            // 
+            // A3
+            // 
+            this.A3.Location = new System.Drawing.Point(172, 0);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(80, 66);
+            this.A3.TabIndex = 2;
+            this.A3.UseVisualStyleBackColor = true;
+            this.A3.Click += new System.EventHandler(this.A3_Click);
+            // 
+            // B3
+            // 
+            this.B3.Location = new System.Drawing.Point(172, 72);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(80, 66);
+            this.B3.TabIndex = 5;
+            this.B3.UseVisualStyleBackColor = true;
+            this.B3.Click += new System.EventHandler(this.B3_Click);
+            // 
+            // B2
+            // 
+            this.B2.Location = new System.Drawing.Point(86, 72);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(80, 66);
+            this.B2.TabIndex = 4;
+            this.B2.UseVisualStyleBackColor = true;
+            this.B2.Click += new System.EventHandler(this.B2_Click);
+            // 
+            // B1
+            // 
+            this.B1.Location = new System.Drawing.Point(0, 72);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(80, 66);
+            this.B1.TabIndex = 3;
+            this.B1.UseVisualStyleBackColor = true;
+            this.B1.Click += new System.EventHandler(this.B1_Click);
+            // 
+            // C3
+            // 
+            this.C3.Location = new System.Drawing.Point(172, 144);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(80, 66);
+            this.C3.TabIndex = 8;
+            this.C3.UseVisualStyleBackColor = true;
+            this.C3.Click += new System.EventHandler(this.C3_Click);
+            // 
+            // C2
+            // 
+            this.C2.Location = new System.Drawing.Point(86, 144);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(80, 66);
+            this.C2.TabIndex = 7;
+            this.C2.UseVisualStyleBackColor = true;
+            this.C2.Click += new System.EventHandler(this.C2_Click);
+            // 
+            // C1
+            // 
+            this.C1.Location = new System.Drawing.Point(0, 144);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(80, 66);
+            this.C1.TabIndex = 6;
+            this.C1.UseVisualStyleBackColor = true;
+            this.C1.Click += new System.EventHandler(this.C1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(271, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(121, 66);
+            this.panel1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.GrayText;
+            this.textBox2.Location = new System.Drawing.Point(0, 36);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(118, 27);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(121, 30);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Turn";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Location = new System.Drawing.Point(271, 105);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(121, 46);
+            this.panel2.TabIndex = 10;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel3.Location = new System.Drawing.Point(3, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(56, 43);
+            this.panel3.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.LimeGreen;
+            this.textBox3.Location = new System.Drawing.Point(271, 79);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(60, 20);
+            this.textBox3.TabIndex = 12;
+            this.textBox3.Text = "Playar 1";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.LimeGreen;
+            this.textBox4.Location = new System.Drawing.Point(332, 79);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(60, 20);
+            this.textBox4.TabIndex = 13;
+            this.textBox4.Text = "Player 2";
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnReset.Location = new System.Drawing.Point(271, 158);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(121, 23);
+            this.btnReset.TabIndex = 14;
+            this.btnReset.Text = "Reset Game";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // btnPlayAgain
+            // 
+            this.btnPlayAgain.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnPlayAgain.Location = new System.Drawing.Point(271, 187);
+            this.btnPlayAgain.Name = "btnPlayAgain";
+            this.btnPlayAgain.Size = new System.Drawing.Size(121, 23);
+            this.btnPlayAgain.TabIndex = 15;
+            this.btnPlayAgain.Text = "Play Again";
+            this.btnPlayAgain.UseVisualStyleBackColor = false;
+            this.btnPlayAgain.Click += new System.EventHandler(this.btnPlayAgain_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel4.Location = new System.Drawing.Point(333, 105);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(56, 43);
+            this.panel4.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 216);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.btnPlayAgain);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Toc Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnPlayAgain;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,477 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tic_toc_toe
+{
+    public partial class Form1 : Form
+    {
+        int counter = 0;
+     
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void A1_Click(object sender, EventArgs e)
+        {
+            
+            if (counter == 0)
+            {
+                A1.Text = "X";
+                counter++;
+                textBox2.Text=("Playar2");
+            }
+            else if (counter == 1)
+            {
+                A1.Text = "O";
+                counter--;
+                textBox2.Text = ("Playar1");
+            }
+            A1.Enabled = false;
+            check();
+        }
+
+        private void A2_Click(object sender, EventArgs e)
+        {
+            if (counter == 0)
+            {
+                A2.Text = "X";
+                counter++;
+                textBox2.Text = ("Playar2");
+            }
+            else if (counter == 1)
+            {
+                A2.Text = "O";
+                counter--;
+                textBox2.Text = ("Playar1");
+            }
+            A2.Enabled = false;
+            check();
+
+
+        }
+
+        private void A3_Click(object sender, EventArgs e)
+        {
+            if (counter == 0)
+            {
+                A3.Text = "X";
+                counter++;
+                textBox2.Text = ("Playar2");
+            }
+            else if (counter == 1)
+            {
+                textBox2.Text = ("Playar2");
+                A3.Text = "O";
+                counter--;
+                
+            }
+            A3.Enabled = false;
+            check();
+
+        }
+
+        private void B1_Click(object sender, EventArgs e)
+        {
+            if (counter == 0)
+            {
+                B1.Text = "X";
+                counter++;
+            }
+            else if (counter == 1)
+            {
+                textBox2.Text = ("Playar2");
+                B1.Text = "O";
+                counter--;
+            }
+            B1.Enabled = false;
+            check();
+        }
+
+        private void B2_Click(object sender, EventArgs e)
+        {
+            if (counter == 0)
+            {
+                B2.Text = "X";
+                counter++;
+            }
+            else if (counter == 1)
+            {
+                B2.Text = "O";
+                counter--;
+            }
+            B2.Enabled = false;
+            check();
+        }
+
+        private void B3_Click(object sender, EventArgs e)
+        {
+            if (counter == 0)
+            {
+                B3.Text = "X";
+                counter++;
+                textBox2.Text = "Playar2";
+            }
+            else if (counter == 1)
+            {
+                B3.Text = "O";
+                counter--;
+            }
+            B3.Enabled = false;
+            check();
+        }
+
+        private void C1_Click(object sender, EventArgs e)
+        {
+            if (counter == 0)
+            {
+                C1.Text = "X";
+                counter++;
+            }
+            else if (counter == 1)
+            {
+                C1.Text = "O";
+                counter--;
+            }
+            C1.Enabled = false;
+            check();
+        }
+
+        private void C2_Click(object sender, EventArgs e)
+        {
+            if (counter == 0)
+            {
+                C2.Text = "X";
+                counter++;
+            }
+            else if (counter == 1)
+            {
+                C2.Text = "O";
+                counter--;
+            }
+            C2.Enabled = false;
+            check();
+        }
+
+        private void C3_Click(object sender, EventArgs e)
+        {
+            if (counter == 0)
+            {
+                C3.Text = "X";
+                counter++;
+            }
+            else if (counter == 1)
+            {
+                C3.Text = "O";
+                counter--;
+            }
+            C3.Enabled = false;
+            check();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            A1.Text = "";
+            A1.Enabled = true;
+            A2.Text = "";
+            A2.Enabled = true;
+            A3.Text = "";
+            A3.Enabled = true;
+            B1.Text = "";
+            B1.Enabled = true;
+            B2.Text = "";
+            B2.Enabled = true;
+            B3.Text = "";
+            B3.Enabled = true;
+            C1.Text = "";
+            C1.Enabled = true;
+            C2.Text = "";
+            C2.Enabled = true;
+            C3.Text = "";
+            C3.Enabled = true;
+            MessageBox.Show("Playar 1 turns First");
+            counter = 1;
+
+        }
+
+
+
+        private void btnPlayAgain_Click(object sender, EventArgs e)
+        {
+            A1.Text = "";
+            A1.Enabled = true;
+            A2.Text = "";
+            A2.Enabled = true;
+            A3.Text = "";
+            A3.Enabled = true;
+            B1.Text = "";
+            B1.Enabled = true;
+            B2.Text = "";
+            B2.Enabled = true;
+            B3.Text = "";
+            B3.Enabled = true;
+            C1.Text = "";
+            C1.Enabled = true;
+            C2.Text = "";
+            C2.Enabled = true;
+            C3.Text = "";
+            C3.Enabled = true;
+            MessageBox.Show("Play again!");
+            MessageBox.Show("Playar 2 turns First");
+            counter = 1;
+        }
+  
+
+        public void check()
+        {
+
+            if (A1.Text != "" && A2.Text != "" && A3.Text != "" &&
+                B1.Text != "" && B2.Text != "" && B3.Text != "" &&
+                C1.Text != "" && C2.Text != "" && C3.Text != "")
+            {
+                MessageBox.Show("Tied");
+            }
+
+
+            if (A1.Text == "X" && B2.Text == "X" && C3.Text == "X")
+            {
+                MessageBox.Show("Player 1 wins");
+                A1.Enabled = false;
+                A2.Enabled = false;
+                A3.Enabled = false;
+                B1.Enabled = false;                 
+                B2.Enabled = false;
+                B3.Enabled = false;
+                C1.Enabled = false;
+                C2.Enabled = false;
+                C3.Enabled = false;
+            }
+            if (A3.Text == "X" && B2.Text == "X" && C1.Text == "X")
+            {
+                MessageBox.Show("Player 1 wins");
+                A1.Enabled = false;
+                A2.Enabled = false;
+                A3.Enabled = false;
+                B1.Enabled = false;
+                B2.Enabled = false;
+                B3.Enabled = false;
+                C1.Enabled = false;
+                C2.Enabled = false;
+                C3.Enabled = false;
+            }
+
+            if (A1.Text == "X" && A2.Text == "X" && A3.Text == "X")
+            {
+                MessageBox.Show("Player 1 wins");
+                A1.Enabled = false;
+                A2.Enabled = false;
+                A3.Enabled = false;
+                B1.Enabled = false;
+                B2.Enabled = false;
+                B3.Enabled = false;
+                C1.Enabled = false;
+                C2.Enabled = false;
+                C3.Enabled = false;
+            }
+            if (B1.Text == "X" && B2.Text == "X" && B3.Text == "X")
+            {
+                MessageBox.Show("Player 1 wins");
+                A1.Enabled = false;
+                A2.Enabled = false;
+                A3.Enabled = false;
+                B1.Enabled = false;
+                B2.Enabled = false;
+                B3.Enabled = false;
+                C1.Enabled = false;
+                C2.Enabled = false;
+                C3.Enabled = false;
+            }
+            if (C1.Text == "X" && C2.Text == "X" && C3.Text == "X")
+            {
+                MessageBox.Show("Player 1 wins");
+                A1.Enabled = false;
+                A2.Enabled = false;
+                A3.Enabled = false;
+                B1.Enabled = false;
+                B2.Enabled = false;
+                B3.Enabled = false;
+                C1.Enabled = false;
+                C2.Enabled = false;
+                C3.Enabled = false;
+            }
+
+            if (A1.Text == "X" && B1.Text == "X" && C1.Text == "X")
+            {
+                MessageBox.Show("Player 1 wins");
+                A1.Enabled = false;
+                A2.Enabled = false;
+                A3.Enabled = false;
+                B1.Enabled = false;
+                B2.Enabled = false;
+                B3.Enabled = false;
+                C1.Enabled = false;
+                C2.Enabled = false;
+                C3.Enabled = false;
+            }
+            if (A2.Text == "X" && B2.Text == "X" && C2.Text == "X")
+            {
+                MessageBox.Show("Player 1 wins");
+                A1.Enabled = false;
+                A2.Enabled = false;
+                A3.Enabled = false;
+                B1.Enabled = false;
+                B2.Enabled = false;
+                B3.Enabled = false;
+                C1.Enabled = false;
+                C2.Enabled = false;
+                C3.Enabled = false;
+                if (A3.Text == "X" && B3.Text == "X" && C3.Text == "X")
+                {
+                    MessageBox.Show("Player 1 wins");
+                    A1.Enabled = false;
+                    A2.Enabled = false;
+                    A3.Enabled = false;
+                    B1.Enabled = false;
+                    B2.Enabled = false;
+                    B3.Enabled = false;
+                    C1.Enabled = false;
+                    C2.Enabled = false;
+                    C3.Enabled = false;
+                }
+
+ 
+
+                if (A1.Text == "O" && B2.Text == "O" && C3.Text == "O")
+                {
+                    MessageBox.Show("Player 2 wins");
+                    A1.Enabled = false;
+                    A2.Enabled = false;
+                    A3.Enabled = false;
+                    B1.Enabled = false;
+                    B2.Enabled = false;
+                    B3.Enabled = false;
+                    C1.Enabled = false;
+                    C2.Enabled = false;
+                    C3.Enabled = false;
+                }
+                if (A3.Text == "O" && B2.Text == "O" && C1.Text == "O")
+                {
+                    MessageBox.Show("Player 2 wins");
+                    A1.Enabled = false;
+                    A2.Enabled = false;
+                    A3.Enabled = false;
+                    B1.Enabled = false;
+                    B2.Enabled = false;
+                    B3.Enabled = false;
+                    C1.Enabled = false;
+                    C2.Enabled = false;
+                    C3.Enabled = false;
+                }
+
+                if (A1.Text == "O" && A2.Text == "O" && A3.Text == "O")
+                {
+                    MessageBox.Show("Player 2 wins");
+                    A1.Enabled = false;
+                    A2.Enabled = false;
+                    A3.Enabled = false;
+                    B1.Enabled = false;
+                    B2.Enabled = false;
+                    B3.Enabled = false;
+                    C1.Enabled = false;
+                    C2.Enabled = false;
+                    C3.Enabled = false;
+                }
+                if (B1.Text == "O" && B2.Text == "O" && B3.Text == "O")
+                {
+                    MessageBox.Show("Player 2 wins");
+                    A1.Enabled = false;
+                    A2.Enabled = false;
+                    A3.Enabled = false;
+                    B1.Enabled = false;
+                    B2.Enabled = false;
+                    B3.Enabled = false;
+                    C1.Enabled = false;
+                    C2.Enabled = false;
+                    C3.Enabled = false;
+                }
+                if (C1.Text == "O" && C2.Text == "O" && C3.Text == "O")
+                {
+                    MessageBox.Show("Player 2 wins");
+                    A1.Enabled = false;
+                    A2.Enabled = false;
+                    A3.Enabled = false;
+                    B1.Enabled = false;
+                    B2.Enabled = false;
+                    B3.Enabled = false;
+                    C1.Enabled = false;
+                    C2.Enabled = false;
+                    C3.Enabled = false;
+
+                }
+
+                if (A1.Text == "O" && B1.Text == "O" && C1.Text == "O")
+                {
+                    MessageBox.Show("Player 2 wins");
+                    A1.Enabled = false;
+                    A2.Enabled = false;
+                    A3.Enabled = false;
+                    B1.Enabled = false;
+                    B2.Enabled = false;
+                    B3.Enabled = false;
+                    C1.Enabled = false;
+                    C2.Enabled = false;
+                    C3.Enabled = false;
+                }
+
+                if (A2.Text == "O" && B2.Text == "O" && C2.Text == "O")
+                {
+                    MessageBox.Show("Player 2 wins");
+                    A1.Enabled = false;
+                    A2.Enabled = false;
+                    A3.Enabled = false;
+                    B1.Enabled = false;
+                    B2.Enabled = false;
+                    B3.Enabled = false;
+                    C1.Enabled = false;
+                    C2.Enabled = false;
+                    C3.Enabled = false;
+                }
+                if (A3.Text == "O" && B3.Text == "O" && C3.Text == "O")
+                {
+                    MessageBox.Show("Player 2 wins");
+                    A1.Enabled = false;
+                    A2.Enabled = false;
+                    A3.Enabled = false;
+                    B1.Enabled = false;
+                    B2.Enabled = false;
+                    B3.Enabled = false;
+                    C1.Enabled = false;
+                    C2.Enabled = false;
+                    C3.Enabled = false;
+                }
+
+
+             
+
+            }
+
+        }
+      
+    }
+}
+            
+        
+    
+
